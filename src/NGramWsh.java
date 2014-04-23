df469d41bae4e952ae18778f64cd9ded59edb81a
Marchorse <marchorse.zhang@gmail.com>
2014-04-23 14:38:01 +0000

    修复语言模型计算条件概率的错误，为什么阿帕格式低级元组有的没有回退权重

--- src/NGramWsh.java
+++ src/NGramWsh.java
@@ -71,7 +71,7 @@ public class NGramWsh {
             }
 		}
 	
-	
+	//当下语言模型的条件概率计算错误
 	public Double cal3(String s1,String s2,String s3) {
 		if (s1.length()==0||s2.length()==0||s3.length()==0)
 			return punish;
